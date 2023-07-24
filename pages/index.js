0d699c7afc72e2943545ffd6512ca4910dc51520
Renan <renansouza2310@gmail.com>
2023-07-24 20:43:22 -0300

    Adiciona a pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <h1>Aqui vai uma frase de efeito.</h1>
+}
+
+export default Home; //esta linha sinaliza para o Next que esta é a função que ele deve renderizar a página no navegador
