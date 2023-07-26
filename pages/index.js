f80f287ac3ec738c31acfd5aeb6b2f5eed073578
Renan <renansouza2310@gmail.com>
2023-07-25 21:44:26 -0300

    Modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 const Home = () => {
-  return <h1>Aqui vai uma frase de efeito.</h1>
+  return <h1>Olá, mundo!!!</h1>
 }
 
 export default Home; //esta linha sinaliza para o Next que esta é a função que ele deve renderizar a página no navegador
