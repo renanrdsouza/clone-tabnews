cdfcc5c30b55eb350c8a5dac54b400894d6921ed
Renan <renansouza2310@gmail.com>
2023-07-28 17:51:23 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-const Home = () => {
-  return <h1>Olá, mundo!!!</h1>
+function Home() {
+  return <h1>Olá, mundo!!!</h1>;
+}
+
+function teste() {
+  console.log("teste");
 }
 
 export default Home; //esta linha sinaliza para o Next que esta é a função que ele deve renderizar a página no navegador
