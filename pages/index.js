bfa11a8fd2484f5889c4d79bb619096de9f0c02e
Renan <renansouza2310@gmail.com>
2023-07-28 18:00:33 -0300

    adiciona o arquivo `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,4 @@ function Home() {
   return <h1>Olá, mundo!!!</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-export default Home; //esta linha sinaliza para o Next que esta é a função que ele deve renderizar a página no navegador
+export default Home;
