eb786c597b9ff57b6febcba4a2fe84cb18e6ef16
Renan <renansouza2310@gmail.com>
2023-10-31 11:26:32 -0300

    add `database.js` e `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,9 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as SUM_RESULT;");
+  console.log(result.rows);
+
   response
     .status(200)
     .json({ message: "alunos do curso.dev são pessoas acima da média" });
