86d6e570d46959ea6a29dccf0c330092714c326f
Renan <renansouza2310@gmail.com>
2023-11-21 16:40:23 -0300

    adiciona scripts para subir os serviços da aplicação

--- infra/database.js
+++ infra/database.js
@@ -6,7 +6,7 @@ async function query(queryObject) {
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD
+    password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();
   const result = await client.query(queryObject);
