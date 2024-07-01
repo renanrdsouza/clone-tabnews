3249373f5bcabdb425af1d41333e2912b9ab1570
Renan Rocha <renansouza2310@gmail.com>
2024-07-01 19:25:12 -0300

    Refatora e estabiliza testes locais.

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,14 +1,14 @@
 import database from "infra/database";
+import orchestrator from "tests/orchestrator.js";
 
-beforeAll(cleanDatabase);
-
-async function cleanDatabase() {
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
   await database.query("drop schema public cascade; create schema public;");
-}
+});
 
 test("POST to /api/v1/migrations should return 200", async () => {
   const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST"
+    method: "POST",
   });
   expect(response1.status).toBe(201);
 
@@ -18,7 +18,7 @@ test("POST to /api/v1/migrations should return 200", async () => {
   expect(responseBody1.length).toBeGreaterThan(0);
 
   const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST"
+    method: "POST",
   });
   expect(response2.status).toBe(200);
 
